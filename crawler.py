8d2fb59c5b0b935a867ef33da3b6fdf98a253fb4
saharzargarzadeh <sahar.zargarzadeh76@gmail.com>
2017-08-03 13:14:19 +0430

    Refactoring.

--- crawler.py
+++ crawler.py
@@ -1,59 +1,64 @@
 import AdvancedHTMLParser as AdvancedHTMLParser
 import urllib
 
+
 def crawl(address):
     response = urllib.urlopen(address)
-    html = response.read()
+    html_of_the_page = response.read()
 
-    parser = AdvancedHTMLParser.AdvancedHTMLParser()
+    html_parser = AdvancedHTMLParser.AdvancedHTMLParser()
 
-    # Parse an HTML string into the document
+    html_parser.parseStr(html_of_the_page)
 
-    parser.parseStr(html)
-    # print(parser.getHTML())
-    # Parse an HTML file into the document
+    package_class = html_parser.getElementsByClassName('package')
+    links = package_class[0][3]
 
-    # parser.parseFile("index.php")
-    temp = parser.getElementsByClassName('package')
-    temp2 = temp[0][3]
+    link_dic = {}
+    link_dic[address] = {}
 
-    linkDic = {}
-    linkDic[address] = {}
+    for i in range(1, len(links.children), 3):
+        link_dic[address][links[i - 1].text] = links[i].__getattribute__('href')
 
-    for i in range(1, len(temp2.children), 3):
-        linkDic[address][temp2[i-1].text] = temp2[i].__getattribute__('href')
-    temp2 = temp[0][4]
-    print(temp2[0].__getattribute__('href'))
-    temp = parser.getElementsByClassName('package-versions-list')
-    versionDic = {}
-    versionDic[address] = []
+    wiki_address = package_class[0][4][0].__getattribute__('href')
+    package_version_class = html_parser.getElementsByClassName('package-versions-list')
+    version_dic = {}
+    version_dic[address] = []
 
-    for i in range(len(temp[0].children)):
-        versionDic[address] += [{}]
-        temp2 = temp[0][i].getElementsByClassName("package-version-header")
-        version = temp2[0][0].text.split()
-        addedOn = temp2[0].text.split()
+    for i in range(len(package_version_class[0].children)):
+        version_dic[address] += [{}]
+        package_version_header_class = package_version_class[0][i].getElementsByClassName("package-version-header")
+        version = package_version_header_class[0][0].text.split()
+        added_on = package_version_header_class[0].text.split()
+
+        n = len(added_on[5])
+        added_on[5] = added_on[5][0: n - 1]
+        added_on = str(added_on[4]) + "-" + str(added_on[5]) + "-" + str(added_on[6])
+
         # TODO addedOn split
-        temp2 = temp[0][i].getElementsByClassName("package-version-permissions")
-        permissionList = []
-        for j in range(len(temp2[0][1].children)):
-            permissionList += [temp2[0][1][j].text.split()[0]]
+        package_version_permission_class = package_version_class[0][i].getElementsByClassName(
+            "package-version-permissions")
+        permission_list = []
+        for j in range(len(package_version_permission_class[0][1].children)):
+            permission_list += [package_version_permission_class[0][1][j].text.split()[0]]
 
-        temp2 = temp[0][i].getElementsByClassName("package-version-source")
-        sourceTarball = temp2[0][0].__getattribute__('href')
-        temp2 = temp[0][i].getElementsByClassName("package-version-download")
-        apkAddress = temp2[0][0].__getattribute__('href')
-        pgpSignature = temp2[0][1].__getattribute__('href')
-        temp2 = temp[0][i].getElementsByClassName("package-version-requirement")
-        requirementVersion = temp2[0].text.split()[4]
+        package_version_source = package_version_class[0][i].getElementsByClassName("package-version-source")
+        source_tarball = package_version_source[0][0].__getattribute__('href')
+        package_version_download = package_version_class[0][i].getElementsByClassName("package-version-download")
+        apk_address = package_version_download[0][0].__getattribute__('href')
+        pgp_signature = package_version_download[0][1].__getattribute__('href')
+        package_version_requirement = package_version_class[0][i].getElementsByClassName("package-version-requirement")
+        requirement_version = package_version_requirement[0].text.split()[4]
 
-        versionDic[address][i]['version'] = version
-        versionDic[address][i]['addedOn'] = addedOn
-        versionDic[address][i]['permissionsList'] = permissionList
-        versionDic[address][i]['sourceTarball'] = sourceTarball
-        versionDic[address][i]['apkAddress'] = apkAddress
-        versionDic[address][i]['pgpSignature'] = pgpSignature
-        versionDic[address][i]['requirementVersion'] = requirementVersion
+        version_dic[address][i]['version'] = version
+        version_dic[address][i]['addedOn'] = added_on
+        version_dic[address][i]['permissionsList'] = permission_list
+        version_dic[address][i]['sourceTarball'] = source_tarball
+        version_dic[address][i]['apkAddress'] = apk_address
+        version_dic[address][i]['pgpSignature'] = pgp_signature
+        version_dic[address][i]['requirementVersion'] = requirement_version
+
+    return link_dic, wiki_address, version_dic
+
 
 main_url = 'https://f-droid.org/'
 
@@ -62,19 +67,19 @@ parser = AdvancedHTMLParser.AdvancedHTMLParser()
 response = urllib.urlopen(main_url + '/packages/')
 html = response.read()
 parser.parseStr(html)
-temp = parser.getElementsByClassName("browse-navigation")
+browse_navigation_class = parser.getElementsByClassName("browse-navigation")
 
-pageCount = int(temp[0][-2][0].text)
-for page in range (1, pageCount + 1):
+pageCount = int(browse_navigation_class[0][-2][0].text)
+for page in range(1, pageCount + 1):
     if page == 1:
         response = urllib.urlopen(main_url + '/packages/')
     else:
         response = urllib.urlopen(main_url + '/packages/' + str(page))
     html = response.read()
     parser.parseStr(html)
-    temp = parser.getElementsByClassName("post-content")
-    temp = temp.getElementsByClassName("package-header")
-    url_size = len(temp)
-    print(url_size)
-    for i in range(min(30, url_size)):
-crawl(main_url + temp[i].__getattribute__('href'))
+    post_content_class = parser.getElementsByClassName("post-content")
+    package_header_class = post_content_class.getElementsByClassName("package-header")
+    url_size = len(package_header_class)
+    for i in range(url_size):
+        crawl(main_url + package_header_class[i].__getattribute__('href'))
+        # TODO saving the output in a data set
